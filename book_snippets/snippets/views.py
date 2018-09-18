7eff62846e64d9c5e0884d7540d1772b1460d38e
Elie Vogel <vogel.elie@gmail.com>
2018-09-18 08:21:17 +0100

    Initial django structure with snippets app added

---
+++ book_snippets/snippets/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello world. You are at the snippets app.")
+
+# Class based views
+
+# See all snippets
+
+# See one snippet
+
+# See all books snippets are from
+
+# See all authors snippets are from
+
+# Search snippets
+
+# Subscribe to snippets email
