6707ffadc108d4d3c707e741c9fa09f5a217c439
Elie Vogel <elie@umed.org>
2018-09-29 12:49:00 +0100

    Add necessary max_length to char fields

--- book_snippets/snippets/models.py
+++ book_snippets/snippets/models.py
@@ -2,10 +2,12 @@ from django.db import models
 from extended_choices import Choices
 from datetime import datetime
 
+
 # Create your models here.
 class Author(models.Model):
     name = models.fields.CharField(
-        "Author's full name"
+        "Author's full name",
+        max_length=255
     )
     birth_year = models.fields.SmallIntegerField()
     death_year = models.fields.SmallIntegerField()
@@ -19,7 +21,9 @@ class Source(models.Model):
         ('BOOK', 1, 'book'),
     )
 
-    title = models.fields.CharField()
+    title = models.fields.CharField(
+        max_length=255
+    )
     authors = models.ManyToManyField(Author)
     date_published = models.fields.DateField()
     type = models.PositiveSmallIntegerField(
@@ -29,20 +33,23 @@ class Source(models.Model):
 
     def __str__(self):
         if self.authors.count == 1:
-            return ("%s by %s") % (self.title, self.author)
+            return ("%s by %s") % (self.title, self.authors.objects.first())
+
 
 class Tag(models.Model):
     """
     Tags
     """
     code = models.fields.SlugField(primary_key=True)
-    name = models.fields.CharField()
+    name = models.fields.CharField(max_length=255)
     description = models.fields.TextField()
 
     def __str__(self):
         return self.name
 
+
 class Snippet(models.Model):
     snippet_text = models.fields.TextField()
-    source = models.ForeignKey(Source)
-    date_added = models.fields.DateField(default=datetime.now) # callable, so get correct datetime
+    source = models.ForeignKey(Source, on_delete=models.PROTECT)
+    date_added = models.fields.DateField(default=datetime.now)  # callable, so get correct datetime
+    tags = models.ManyToManyField(Tag)
