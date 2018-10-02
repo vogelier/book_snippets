afb2890358c7c4b5b3a355ad07bdc49fe5295406
Elie Vogel <elie@umed.org>
2018-10-02 12:06:52 +0100

    More fleshed out models

--- book_snippets/snippets/models.py
+++ book_snippets/snippets/models.py
@@ -3,19 +3,30 @@ from django.utils.translation import ugettext_lazy as _
 
 from extended_choices import Choices
 from datetime import datetime
+from model_utils.models import StatusModel
 
-
+# TODO - look at url model mixin or is it same as slug mixin?
+# TODO - do we need a mixin for model creation/update
 # Create your models here.
+
+
 class Author(models.Model):
-    name = models.fields.CharField(
-        _('Author full name'),
+    given_name = models.fields.CharField(
         max_length=255
     )
-    birth_year = models.fields.SmallIntegerField()
-    death_year = models.fields.SmallIntegerField()
+    middle_name = models.fields.CharField(
+        max_length=255,
+        blank=True,
+        default=''
+    )
+    family_name = models.fields.CharField(
+        max_length=255
+    )
+    birth_year = models.fields.SmallIntegerField(blank=True, default=0)
+    death_year = models.fields.SmallIntegerField(blank=True, default=0)
 
     def __str__(self):
-        return self.name
+        return ("%s %s") % (self.given_name, self.family_name)
 
 
 class Source(models.Model):
@@ -27,16 +38,35 @@ class Source(models.Model):
     title = models.fields.CharField(
         max_length=255
     )
-    authors = models.ManyToManyField(Author)
-    date_published = models.fields.DateField()
+    authors = models.ManyToManyField(Author, related_name='all_work')
+    date_published = models.fields.DateField(blank=True)
     type = models.PositiveSmallIntegerField(
         choices=PUBLICATION_TYPES,
         default=PUBLICATION_TYPES.BOOK
     )
 
     def __str__(self):
-        if self.authors.count == 1:
-            return _("%s by %s") % (self.title, self.authors.objects.first())
+        return self.title
+
+
+class ReadingStatus(StatusModel, models.Model):
+    STATUS = Choices(
+        ('NOW_READING', 'now reading', 'Now reading'),
+        ('READ', 'read', 'Read'),
+        ('STALLED', 'stalled', 'Stalled'),
+        ('TO_READ', 'to read', 'To Read')
+    )
+    source = models.ForeignKey(Source, on_delete=models.CASCADE)
+
+
+class KnowledgeTransferStatus(StatusModel, models.Model):
+    STATUS = Choices(
+        ('TRANSFER_DONE', 'transferred', 'Transferred'),
+        ('IN_PROGRESS', 'in progress', 'In Progress'),
+        ('NOT_STARTED', 'not started', 'Not Started'),
+        ('STALLED', 'stalled', 'Stalled'),
+    )
+    source = models.ForeignKey(Source, on_delete=models.CASCADE)
 
 
 class Tag(models.Model):
@@ -45,14 +75,35 @@ class Tag(models.Model):
     """
     code = models.fields.SlugField(primary_key=True)
     name = models.fields.CharField(max_length=255)
-    description = models.fields.TextField()
+    description = models.fields.TextField(blank=True)
 
     def __str__(self):
-        return self.name
+        return self.code
 
 
 class Snippet(models.Model):
-    snippet_text = models.fields.TextField()
-    source = models.ForeignKey(Source, on_delete=models.PROTECT)
+    LENGTH_LIMIT = 30
+    text = models.fields.TextField()
+    source = models.ForeignKey(
+        Source,
+        on_delete=models.SET_NULL,
+        null=True
+    )
     date_added = models.fields.DateField(default=datetime.now)  # callable, so get correct datetime
-    tags = models.ManyToManyField(Tag)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        if not len(self.text.__str__()) > self.LENGTH_LIMIT:
+            return self.text.__str__()
+        return self.text.__str__()[:self.LENGTH_LIMIT] + '...'
+
+
+class IndexEntry(models.Model):
+    page = models.IntegerField()
+    topic = models.ForeignKey(
+        Tag,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    note = models.TextField(blank=True)
+    source = models.ForeignKey(Source, on_delete=models.CASCADE, related_name='indexes')
--- book_snippets/snippets/urls.py
+++ book_snippets/snippets/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/create', views.CreateSnippetView.as_view(), name='create-snippet'),
+    path('create', views.CreateSnippetView.as_view(), name='create-snippet'),
 
 ]
